efe5fec175760c58a69a7d7fc4a161e566db03d6
cypherz01 <mrtomcuthbertson@gmail.com>
2021-10-12 10:20:57 +0000

    initialize project
    -spring web (endpoints)
    -spring data jpa (easy db access)
    -h2 (in-memory db)
    -lombok (no getters/setters)

---
+++ src/main/java/com/example/hobbitrun/HobbitrunApplication.java
@@ -0,0 +1,13 @@
+package com.example.hobbitrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbitrunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbitrunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hobbitrun/HobbitrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hobbitrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbitrunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
