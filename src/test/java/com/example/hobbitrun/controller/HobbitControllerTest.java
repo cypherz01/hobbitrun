ba8a9675bee7fac6dbbb60db54aa68a30b6b9945
cypherz01 <mrtomcuthbertson@gmail.com>
2021-10-13 10:13:19 +0000

    added test to check size of sql database

--- src/test/java/com/example/hobbitrun/controller/HobbitControllerTest.java
+++ src/test/java/com/example/hobbitrun/controller/HobbitControllerTest.java
@@ -1,4 +1,7 @@
 package com.example.hobbitrun.controller;
+import com.example.hobbitrun.model.Hobbit;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 // Use spring context for tests
 @SpringBootTest
 // Give some info to Spring on how to create MockMvc
@@ -17,6 +26,8 @@ class HobbitControllerTest {
     @Autowired
     private MockMvc mockMvc;
     //Regular JUnit
+    @Autowired
+    private ObjectMapper objectMapper;
     @Test
     //JUnit5 feature
     @DisplayName("GET /hobbits -> HTTP 200")
@@ -36,6 +47,24 @@ class HobbitControllerTest {
                 // when calling /hobbit endpoint
                 .perform(MockMvcRequestBuilders.post("/hobbits"))
                 // then we should get HTTP 200
-                .andExpect(MockMvcResultMatchers.status().is(405));
+                .andExpect(status().is(405));
+    }
+
+    @Test
+    void given3hobbits_whenGETHobbits_thenHTTP200AndListSize3() throws Exception{
+
+        final var mvcResult= mockMvc
+                // when calling /hobbit endpoint
+                .perform(get("/hobbits"))
+                .andDo(print())
+                // then we should get HTTP 200
+                .andExpect(status().is(200))
+                .andReturn();
+
+        final var hobbitsFromDbinJSONFormat=mvcResult.getResponse().getContentAsString();
+
+        List<Hobbit> hobbitsFromDB = objectMapper.readValue(hobbitsFromDbinJSONFormat, new TypeReference<>() {});
+
+        assertEquals(3,hobbitsFromDB.size());
     }
 }
