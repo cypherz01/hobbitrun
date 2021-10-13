1cdf921888fda5d1266551ed815d214957a6b39d
cypherz01 <mrtomcuthbertson@gmail.com>
2021-10-13 09:45:53 +0000

    added a data sql file to populate model

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO hobbit VALUES (1,'Frodo','Baggins');
+INSERT INTO hobbit VALUES (2,'Freddo','Baggins');
+INSERT INTO hobbit VALUES (3,'gordo','Baggins');
+INSERT INTO hobbit VALUES (4,'smegol','Baggins');
+INSERT INTO hobbit VALUES (5,'Froggie','Baggins');
